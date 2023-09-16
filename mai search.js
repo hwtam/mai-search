02a764fd91a669f159fb7bb61728a35b25a3afaf
tomtom <112061601+hwtam@users.noreply.github.com>
2023-09-16 16:34:01 +0800

    support mai-tools(Analyze Rating)

--- mai search.js
+++ mai search.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         mai search
-// @version      1.0
+// @version      2.0
 // @description  quick search maimai songs in youtube
 // @author       tomtom
 // @match        https://maimaidx-eng.com/maimai-mobile/record/
+// @match        https://myjian.github.io/mai-tools/rating-calculator/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -13,7 +14,7 @@
 
     var styleElement = document.createElement("style");
     var str = `
-div[class="basic_block m_5 p_5 p_l_10 f_13 break"]::before {
+div[class="basic_block m_5 p_5 p_l_10 f_13 break"]::before, td[class="scoreRecordCell songTitleCell"]::before {
   content: "Search";
   display: inline-block;
   padding: 0px 3px;
@@ -29,6 +30,27 @@ div[class="basic_block m_5 p_5 p_l_10 f_13 break"]::before {
     document.head.appendChild(styleElement);
 
 
+    function removeLV (str) {
+        var arr = str.split(' ');
+        return str.replace('Lv ' + arr[arr.length-1], '');
+    }
+
+    function searchYT (title, diff) {
+        var url = 'https://www.youtube.com/results?search_query=maimai+' + title + ' ' + diff;
+        window.open(url.replace(' ', '+'), "_blank");
+    }
+
+    function addEvent() {
+        var buttons = document.querySelectorAll('td[class="scoreRecordCell songTitleCell"]');
+        buttons.forEach(function(button) {
+            button.addEventListener('click', function() {
+                var title = button.textContent;
+                var diff = button.nextSibling.textContent;
+                searchYT(title, diff);
+            });
+        });
+    }
+
     var buttons = document.querySelectorAll('div[class="basic_block m_5 p_5 p_l_10 f_13 break"]');
     buttons.forEach(function(button) {
         button.addEventListener('click', function() {
@@ -38,16 +60,10 @@ div[class="basic_block m_5 p_5 p_l_10 f_13 break"]::before {
         });
     });
 
-
-    function removeLV (str) {
-        var arr = str.split(' ');
-        return str.replace('Lv ' + arr[arr.length-1], '');
-    }
-
-    function searchYT (title, diff) {
-        var url = 'https://www.youtube.com/results?search_query=maimai+' + title + ' ' + diff;
-        window.open(url.replace(' ', '+'), "_blank");
-        return;
-    }
+    var observer = new MutationObserver(() => {
+        addEvent();
+    });
+    observer.observe(document.body, {childList: true , subtree: true});
+    addEvent();
 
 })();
